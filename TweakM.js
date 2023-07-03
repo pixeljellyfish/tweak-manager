93b412a85670a68892ceecc7c0457b168fb83736
pixeljellyfish <g3npixel@gmail.com>
2023-07-04 02:53:23 +1000

    script is fully working only works if user has defined folder path

--- TweakM.js
+++ TweakM.js
@@ -2,23 +2,33 @@
 
 const readlineSync = require('readline-sync');
 const { execSync } = require('child_process');
+const path = require('path');
 
-function findFile(fileName) {
+function findFolder(folderName) {
   try {
-    const command = `find ~ -type f -name "${fileName}"`;
+    const command = `find . -type d -name "${folderName}"`;
+    console.log(`Executing command: ${command}`);
     const result = execSync(command).toString().trim();
-    return result.split('\n');
+    const folderPaths = result.split('\n').map((folderPath) => folderPath.trim());
+    console.log('Folder paths:', folderPaths);
+    if (folderPaths.length === 0 || folderPaths[0] === '') {
+      console.log(`Folder '${folderName}' not found.`);
+      process.exit(1);
+    } else {
+      console.log(`Folder '${folderName}' found at: ${folderPaths[0]}`);
+      return folderPaths[0]; // Return the folder path
+    }
   } catch (error) {
-    console.error('Error occurred while searching for the file.');
+    console.error('Error occurred while searching for the folder.');
     process.exit(1);
   }
 }
 
-function copyFile(filePath, destination) {
+function copyFile(source, destination) {
   try {
-    const command = `cp "${filePath}" "${destination}"`;
+    const command = `cp "${source}" "${destination}"`;
     execSync(command);
-    console.log(`File '${filePath}' copied to '${destination}'.`);
+    console.log(`File '${source}' copied to '${destination}'.`);
   } catch (error) {
     console.error('Error occurred while copying the file.');
     process.exit(1);
@@ -26,24 +36,31 @@ function copyFile(filePath, destination) {
 }
 
 function main() {
-  console.log('=== File Manager ===');
-  const fileName = readlineSync.question('Enter the name of the file: ');
-
-  const filePaths = findFile(fileName);
-  if (filePaths.length === 0) {
-    console.log(`File '${fileName}' not found.`);
-    process.exit(1);
-  } else if (filePaths.length === 1) {
-    console.log(`File '${fileName}' found at: ${filePaths[0]}`);
-    const destination = readlineSync.question('Enter the destination directory: ');
-    
-    copyFile(filePaths[0], destination);
-  } else {
-    console.log(`Multiple files found with the name '${fileName}':`);
-    console.log(filePaths.join('\n'));
-    console.log('Please refine your search.');
-    process.exit(1);
-  }
+  console.log('=== Tweaks And Repo Manager ===');
+  console.log("starting Tweaks And Repo Manager...")
+  console.log("by PixelJellyfish")
+  console.log("...")
+  console.log("getting latest .deb file...")
+  const tweaksFolderPath = '/Users/pixeljellyfish/c0mebackf0lders/packages/';
+  const repoFolderPath = '/Users/pixeljellyfish/repo/debs'; // Replace with the actual path of the repo folder
+  console.log("Please Wait...")
+  console.log("Executing command: find")
+  const latestDebFileCommand = `find "${tweaksFolderPath}" -type f -name '*.deb' -exec stat -f '%m %N' {} \\; | sort -n | tail -1 | awk '{print $2}'`;
+  console.log("Executing command: find latest .deb file in $repoFolderPath")
+  const source = execSync(latestDebFileCommand).toString().trim();
+  console.log('Latest .deb file:', source);
+  const destination = path.join(repoFolderPath, path.basename(source));
+  copyFile(source, destination);
+  console.log(`Changing directory to: ${repoFolderPath}`);
+  process.chdir(repoFolderPath); // Change the current working directory to the repo folder
+  console.log('Running the .sh script in the repo folder...');
+  const scriptPath = '/Users/pixeljellyfish/repo/scan-packages.sh'; // Replace with the actual path of your .sh script
+  execSync(`sh ${scriptPath}`, { stdio: 'inherit' });
+  console.log('Scan-packages script has completed successfully.');
+  console.log("Repo Has Been Updated!")
+  console.log("Thank You For Using Tweaks And Repo Manager!")
+  console.log("Exiting...")
+  process.exit(0);
 }
 
 main();
